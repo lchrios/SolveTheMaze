10210a2e906819cbb86cd1ee5a761f18ec81dfc5
Christopher Ortega <chrortegita@gmail.com>
2023-03-11 14:06:54 -0700

    orientation enum fixed

--- src/com/cool/fun/stuff/Orientation.java
+++ src/com/cool/fun/stuff/Orientation.java
@@ -1,20 +1,5 @@
 package com.cool.fun.stuff;
 
-public enum Side {
-    TOP(0, -1),
-    LEFT(-1, 0),
-    BOTTOM(0, 1),
-    RIGHT(1, 0);
-
-    private Integer dx;
-    private Integer dy;
-
-    private Side(Integer dx, Integer dy) { this.dx = dx; this.dy = dy; }
-
-    public Integer getDx() { return this.dx; }
-    public Integer getDy() { return this.dy; }
-}
-
 public enum Orientation {
         NORTH(0, Side.RIGHT, Side.TOP, Side.LEFT),
         WEST(1, Side.TOP, Side.LEFT, Side.BOTTOM),
@@ -32,4 +17,28 @@ public enum Orientation {
     public Side getLeft() { return this.left; }
     public Side getFront() { return this.front; }
     public Side getRight() { return this.right; }
+
+    public static Orientation getOrientation(Integer code) {
+        for (Orientation or : Orientation.values()) {
+            if (or.code == code) return or;
+        }
+
+        return null;
+    }
+
+    public enum Side {
+        TOP(0, -1),
+        LEFT(-1, 0),
+        BOTTOM(0, 1),
+        RIGHT(1, 0);
+    
+        private Integer dx;
+        private Integer dy;
+    
+        private Side(Integer dx, Integer dy) { this.dx = dx; this.dy = dy; }
+    
+        public Integer getDx() { return this.dx; }
+        public Integer getDy() { return this.dy; }
+    }
+
 }
